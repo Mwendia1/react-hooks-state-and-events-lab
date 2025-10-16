96888977a1020363e6acbdb841688d83243cc860
Mwendia1 <elijah.mwendia@student.moringaschool.com>
2025-10-16 15:00:40 +0300

    .

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ function App() {
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
    // this will be used for the Dark Mode Toggle feature
+  
   const[isDarkMode, setIsDarkMode]=useState(false);
   const appClass = isDarkMode? "App dark" : "App light"
   function handleToggle(){
